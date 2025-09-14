76387e45c363cfab964ba1b1914063a90a7b23a5
zeronixgrave12 <zeronixgrave@gmail.com>
2025-09-14 19:19:43 +0600

    Create Offbot.js

---
+++ scripts/cmds/Offbot.js
@@ -0,0 +1,20 @@
+module.exports = {
+  config: {
+    name: "offbot",
+    version: "1.0",
+    author: "sajin",
+    countDown: 45,
+    role: 0,
+    shortDescription: "Turn off bot",
+    longDescription: "Turn off bot",
+    category: "owner",
+    guide: "{p}{n}"
+  },
+  onStart: async function ({event, api}) {
+    const permission = [ "100074722578899" ];
+  if (!permission.includes(event.senderID)) {
+    api.sendMessage("╔════ஜ۩۞۩ஜ═══╗\nYou don't have permission to use this command.\n═══ஜ۩۞۩ஜ═══╝", event.threadID, event.messageID);
+    return;
+  }
+    api.sendMessage("╔════ஜ۩۞۩ஜ═══╗\nsuccessfully Turned Off System ✅\═══ஜ۩۞۩ஜ═══╝",event.threadID, () =>process.exit(0))}
+};
